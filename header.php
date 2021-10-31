b46c9d92d8602dda760697877308c66b6d30b02c
sina majidi <sin.majidi@gmail.com>
2021-11-01 01:28:36 +0330

    lets make a weblog using html,css and php

---
+++ header.php
@@ -0,0 +1,15 @@
+<ul>
+  <li><a href="weblog.php">خانه</a></li>
+  <li><a href="#news">دسته</a></li>
+  <!-- <li class="dropdown">
+    <a  class="dropbtn">مقالات</a>
+    <div class="dropdown-content">
+      <a href="#link1">لینک 1</a>
+      <a href="#link2">لینک 2</a>
+      <a href="#link3">لینک 3</a>
+    </div>
+  </li> -->
+  <li class="logo"><a href="weblog.php">لوگو</a></li>
+  <li class="login"><a href="login.php">ورود</a></li>
+  <li class="signup"><a href="register.php">ثبت نام</a></li> 
+</ul>
---
+++ register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+  <title>وبلاگ</title>
+  <link rel="stylesheet" href="css/styles.css">
+  
+</head>
+<header>
+<?php
+include_once('header.php');
+?>
+</header>
+
+<body>
+
+
+  
+<div class="container">
+  <h1> ثبت نام</h1>
+
+</div>
+
+</body>
+
+</html>
