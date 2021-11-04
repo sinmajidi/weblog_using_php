fa3533f589d9ce80a7be82d6d6d21cdf5ebc4b02
sina majidi <sin.majidi@gmail.com>
2021-11-04 22:40:56 +0330

    pluse corses page with design

--- courses.php
+++ courses.php
@@ -3,29 +3,130 @@
 <head>
   <title>وبلاگ</title>
   <link rel="stylesheet" href="css/styles.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
 
+</head>
 <header>
-    <?php
-        include_once('header.php');
-    ?>
+<?php
+include_once('header.php');
+?>
 </header>
 
 <body>
 
   
-    <div class="container">
-        <h1> دوره های آموزشی</h1>
+    <div class="stuff_box">
+    <div class="card">
+        <div class="face face1">
+          <div class="content">
+            <h3>نام دوره</h3>
+          </div>
+        </div>
+        <div class="face face2">
+          <div class="content">
+            <p>
+             معرفی دوره
+              
+            </p>
+            <a href="#" type="button">اضافه به سبد خرید</a>
+          </div>
+        </div>
+      </div>
+      
+
+      <div class="card">
+        <div class="face face1">
+          <div class="content">
+            <h3>نام دوره</h3>
+          </div>
+        </div>
+        <div class="face face2">
+          <div class="content">
+            <p>
+              معرفی دوره
+            
+            </p>
+            <a href="#" type="button">اضافه به سبد خرید</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="face face1">
+          <div class="content">
+            <h3>نام دوره</h3>
+          </div>
+        </div>
+        <div class="face face2">
+          <div class="content">
+            <p>
+              معرفی دوره
+            
+            </p>
+            <a href="#" type="button">اضافه به سبد خرید</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="face face1">
+          <div class="content">
+            <h3>نام دوره</h3>
+          </div>
+        </div>
+        <div class="face face2">
+          <div class="content">
+            <p>
+              معرفی دوره
+            
+            </p>
+            <a href="#" type="button">اضافه به سبد خرید</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="face face1">
+          <div class="content">
+            <h3>نام دوره</h3>
+          </div>
+        </div>
+        <div class="face face2">
+          <div class="content">
+            <p>
+              معرفی دوره
+            
+            </p>
+            <a href="#" type="button">اضافه به سبد خرید</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="face face1">
+          <div class="content">
+            <h3>نام دوره</h3>
+          </div>
+        </div>
+        <div class="face face2">
+          <div class="content">
+            <p>
+              معرفی دوره
+            
+            </p>
+            <a href="#" type="button">اضافه به سبد خرید</a>
+          </div>
+        </div>
+      </div>
 
     </div>
 
-</body>
+    
 
+</body>
 <footer>
-    <?php
-        include_once('footer.php');
-    ?>
+<?php
+include_once('footer.php');
+?>
 </footer>
 
 </html>
