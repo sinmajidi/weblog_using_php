12079934e12efcdbfd9ceee4e55954c13bef385a
sina majidi <sin.majidi@gmail.com>
2021-11-05 23:38:23 +0330

    let's make an admin page to super user to control the users and articles and other controls,so in make a grid with four field in adminpage.php for start design admin page!

--- adminpage.php
+++ adminpage.php
@@ -1,25 +1,33 @@
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
+<link rel="stylesheet" href="css/styles.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
 <body>
-  <p>admin page</P>
-  <?php
- $cookie_name = "user";
  
-if(count($_COOKIE) > 0) {
-    //echo "Cookies are enabled.";
-  } else {
-    //cho "Cookies are disabled.";
-  }
 
-  if(!isset($_COOKIE[$cookie_name])) {
-    //echo "Cookie named '" . $cookie_name . "' is not set!";
-  } else {
-    //echo "Cookie '" . $cookie_name . "' is set!<br>";
-    echo  "wellcome ".$_COOKIE[$cookie_name] ;
-  }
-?>
+<div class="adminpage-container">
+  <div class="item1">Header</div>
+  <div class="item2">
+    <div class="user_list_topic">
+      <p> لیست کاربران</p>
+    </div>
+    <div class="user_list_content">
+      <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto tempora porro nemo, nihil ea odit explicabo odio recusandae. Quam corrupti maiores, rem molestias tempore voluptas nihil ut? Ipsum, esse praesentium?  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia tempore eos itaque minus id atque hic nostrum, nesciunt nulla quae possimus est enim at fugiat debitis veniam. Sed, dolores et!</p>
+    </div>
+  </div>
+  <div class="item3">
+    <div class="articles_list_topic">
+      <p> لیست مقالات</p>
+    </div>
+    <div class="articles_list_content">
+      <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto tempora porro nemo, nihil ea odit explicabo odio recusandae. Quam corrupti maiores, rem molestias tempore voluptas nihil ut? Ipsum, esse praesentium?  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia tempore eos itaque minus id atque hic nostrum, nesciunt nulla quae possimus est enim at fugiat debitis veniam. Sed, dolores et!</p>
+    </div>
+  </div>  
+ 
+  <div class="item5">Footer</div>
+</div>
+
+  
 </html>
